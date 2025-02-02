48e33333e1122754d65a3faed3c374305dac0f2b
Ayub40 <ayubk4028@gmail.com>
2025-02-03 01:34:30 +0600

    Trending Courses Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../../Components/Home/Banner/Banner";
+import TrendingCourses from "../TrendingCourses/TrendingCourses";
 
 
 const Home = () => {
     return (
         <div className="max-w-screen-[1700px] m-auto max-w-7xl">
             <Banner></Banner>
+            <TrendingCourses></TrendingCourses>
         </div>
     );
 };
